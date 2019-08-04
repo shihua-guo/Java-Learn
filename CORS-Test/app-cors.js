033eb740418fe4b0fa91e5a2d114a7b247646f21
guoshihua <2977131415@qq.com>
2019-08-04 22:52:24 +0800

    添加界面例子

--- CORS-Test/app-cors.js
+++ CORS-Test/app-cors.js
@@ -16,5 +16,11 @@ app.post('/data',function(req,resp,next){
 		"age":22
 	});
 });
+app.get('/data',function(req,resp,next){
+	resp.send({
+		"name":"foo",
+		"age":22
+	});
+});
 app.listen(3002);
 console.log("app is running on 3002");
